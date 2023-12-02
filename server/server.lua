1170c435b6670cab28c3f2a9355d74188094eae8
RetryR1v2 <147660115+RetryR1v2@users.noreply.github.com>
2023-12-02 20:59:08 +0100

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -11,7 +11,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/RetryR1v2/mms-luckytickets/main/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://raw.githubusercontent.com/RetryR1v2/mms-drugs/main/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then 
